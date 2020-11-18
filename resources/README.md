b5d160cf5f889c757488a4158f92cc586728c616
mikespub <github@mikespub.net>
2020-11-18 14:52:49 +0000

    start api schemas

---
+++ resources/README.md
@@ -0,0 +1,16 @@
+# SWAPI
+## The Star Wars API 
+
+Source code for [swapi.dev](https://swapi.dev)
+
+
+## What happened to swapi.co? 
+Unfortunately swapi.co is not maintained anymore. 
+
+## What is swapi.dev? 
+An "unofficial" branch of "swapi", supported by @juriy
+
+
+## Will it be updated?
+Original swapi project is built on Python/Django stack that is quite outdated by now. It would be a great exercise to rewrite it in cloud-native manner, but this requires time and effort. I'm looking into doing that. There's no timelines yet, since "swapi" stopped its existence quite suddenly. Suggestions and help are always welcome!
+
---
+++ xarversion.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * API Schemas version information
+ *
+ * @package modules\apischemas
+ * @copyright (C) 2020 Mike's Pub
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link https://github.com/mikespub/xaraya-modules
+ * @author mikespub
+ */
+$modversion = array();
+$modversion['name']           = 'apischemas';
+$modversion['id']             = '18252';
+$modversion['version']        = '2.4.1';
+$modversion['displayname']    = 'API Schemas';
+$modversion['description']    = 'Play with API schemas';
+$modversion['credits']        = '';
+$modversion['help']           = '';
+$modversion['changelog']      = '';
+$modversion['license']        = '';
+$modversion['official']       = false;
+$modversion['author']         = 'mikespub';
+$modversion['contact']        = 'https://github.com/mikespub/xaraya-modules';
+$modversion['admin']          = false;
+$modversion['user']           = false;
+$modversion['class']          = 'Utility';
+$modversion['category']       = 'Miscellaneous';
+$modversion['securityschema'] = array();
+$modversion['dependency']     = array();
+$modversion['dependencyinfo'] = array();
